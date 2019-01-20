14641818c1d58aabe175d6c345a1e8dd4c13ea5f
Jakub Koralewski <jakub.koralewski1@gmail.com>
2019-01-20 14:44:28 +0100

    add svgs to popup;
    make popup prettier;
    
    communicate with content script (box-select.js)

---
+++ popup/popup.js
@@ -0,0 +1,42 @@
+const del = document.querySelector('#delete');
+const boxSelect = document.querySelector('#box-select');
+
+boxSelect.addEventListener('click', () => {
+    boxSelect.classList.add('select-active');
+
+    chrome.tabs.query(
+        {
+            active: true,
+            currentWindow: true
+        },
+        function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                action: 'boxSelect'
+            });
+        }
+    );
+});
+
+del.addEventListener('click', () => {
+    chrome.tabs.query(
+        {
+            active: true,
+            currentWindow: true
+        },
+        function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                action: 'delete'
+            });
+        }
+    );
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action == 'boxSelectOff') {
+        boxSelect.classList.remove('select-active');
+    } else if (request.action == 'deleteStart') {
+        del.classList.add('delete-active');
+    } else if (request.action == 'deleteEnd') {
+        del.classList.remove('delete-active');
+    }
+});
