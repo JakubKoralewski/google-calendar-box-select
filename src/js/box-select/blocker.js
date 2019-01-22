07fc0057239d7a53fab91080c3e6d6ccaf0b5355
Jakub Koralewski <jakub.koralewski1@gmail.com>
2019-01-22 14:01:35 +0100

    add JSDocs to blocker class

--- src/js/box-select/blocker.js
+++ src/js/box-select/blocker.js
@@ -1,17 +1,23 @@
-// Blocker will disable click reaction when holding e.g. 'B'
+/**
+ *  Blocker is a transparent overlay that allows for click and drag.
+ *  Blocker will disable click reaction when holding e.g. 'B'
+ * */
 class Blocker {
     constructor() {
-        this.blocker = document.createElement('div');
-        this.blocker.id = 'blocker';
+        this.element = document.createElement('div');
+        this.element.id = 'blocker';
         this.created = false;
     }
-
+    /** Turn Blocker instance on or off.
+     *  Is being inserted after the first child of parent so that the slidedown element is always above blocker.
+     * @param {string} parent - The parent to become the first child of.
+     * @param {number} state - 0 (off); 1 (on);
+     */
     setState(parent, state) {
         //console.log(`toggle blocker ${state ? 'on' : 'off'}`);
         state
-            ?
-            parent.insertBefore(this.blocker, parent.firstChild) :
-            parent.removeChild(this.blocker);
+            ? parent.insertBefore(this.element, parent.childNodes[1])
+            : parent.removeChild(this.element);
         this.created = state;
     }
 }
