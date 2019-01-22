7df2dbc694d5b241dfcd527cc22888810f0eac4f
Jakub Koralewski <jakub.koralewski1@gmail.com>
2019-01-22 14:02:10 +0100

    use Prettier for with ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,26 @@
 module.exports = {
     "env": {
         "browser": true,
+        "node": true,
         "es6": true
     },
+    "plugins": ["prettier"],
     "extends": "eslint:recommended",
     "parserOptions": {
         "ecmaVersion": 2018,
         "sourceType": "module",
     },
     "rules": {
-        "indent": [
+        "prettier/prettier": [
+            "error",
+            {
+                "singleQuote": true,
+                "semi": true,
+                "tabWidth": 4,
+                "printWidth": 100
+            }
+    ],
+/*         "indent": [
             "warn",
             4
         ],
@@ -20,10 +31,6 @@ module.exports = {
         "quotes": [
             "error",
             "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
+        ] */
     }
 };
