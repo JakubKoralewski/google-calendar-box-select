11ba02a20945fac2bd0391b3b44a54daab4b018d
Jakub Koralewski <jakub.koralewski1@gmail.com>
2019-01-29 18:10:04 +0100

    add config file

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const OK_PATH =
+	'div.I7OXgf.dT3uCc.gF3fI.fNxzgd.Inn9w.iWO5td > div.OE6hId.J9fJmf > div > div.uArJ5e.UQuaGc.kCyAyd.l3F1ye.ARrCac.HvOprf.evJWRb.M9Bg4d';
+export const TRASH_PATH =
+	'#xDetDlg > div > div.Tnsqdc > div > div > div.pPTZAe > div:nth-child(2) > div';
+export const BRIGHTEN_BY = 20;
+export const GRADIENT =
+	'-webkit-linear-gradient(left, ${evtColor} 0%, ${brColor} 50%, ${evtColor} 100%), linear-gradient(to right, ${evtColor} 0%, ${brColor} 50%, ${evtColor} 100%)';
